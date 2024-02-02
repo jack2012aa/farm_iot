95c3ec1ebec89195ce48520e78146f968af8386b
ChangYu Huang <jack.mainwork@gmail.com>
2024-02-02 21:05:08 +0800

    Add a test case.

---
+++ test/feed_scale/exporter.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase): 
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
