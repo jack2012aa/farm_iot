77fe7bae55981d832d919ad4f37ae0e0faf3ea11
ChangYu Huang <jack.mainwork@gmail.com>
2024-02-16 17:01:36 +0800

    Add gateway manager class to manage ports.

---
+++ basic_sensor/gateway.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+
+from pymodbus.client.serial import AsyncModbusSerialClient
+
+from general import type_check
+
+
+@dataclass
+class RTUConnectionSettings:
+    '''
+    * param port: serial port number. Should look like 'COM2', 'COM3'. Please refer to (COM and LPT) section in the Device Manager in Windows.
+    * param baundrate: the baundrate of the modbus device. Please refer to the device document provided by productors. 
+    * param bytesize: modbus setting. Please refer to the device document provided by productors.
+    * param parity: modbus setting. Please refer to the device document provided by productors.
+    * param stopbits: modbus setting. Please refer to the device document provided by productors.
+    * param time_out: modbus setting. Please refer to the device document provided by productors.
+    '''
+
+    PORT: str # port number
+    BAUNDRATE: int = 38400
+    BYTESIZE: int = 8
+    PARITY: str = "N"
+    STOPBITS: int = 1
+    TIME_OUT: int = 5
+
+
+class ModbusRTUGatewayConnectionsManager():
+    '''
+    A class managing connection to RTU gateways. 
+
+
+    Because more than one connection to a port is not allowed, this class maps 
+    each port number to a `AsyncModbusSerialClient` object. \ 
+    Please use the static method `get_connection` to get an client object. \ 
+    DO NOT close the connection using the returned client object.
+    '''
+
+    __connections: dict[str, AsyncModbusSerialClient] = {}
+
+    @staticmethod
+    def get_connection(port: str) -> AsyncModbusSerialClient | None:
+        '''
+        Return a connected `AsyncModbusSerialClient` object. 
+        If the connection does not exist, return None.
+
+        This class maps port number to client object. It does not assure the 
+        equality of baundrate, time out, etc. \ 
+        Please use `create_connection` to create a connection.
+        Please DO NOT close the connection through the returned object.
+        '''
+        type_check(port, "port", str)
+        return ModbusRTUGatewayConnectionsManager.__connections.get(port)
+
+    @staticmethod
+    async def create_connection(settings: RTUConnectionSettings) -> None:
+        '''
+        Create an `AsyncModbusSerialClient` object.
+
+
+        This class maps port number to client object. It does not assure the 
+        equality of baundrate, time out, etc. \ 
+        Please use `get_connection` to receive the created object.
+        '''
+
+        if ModbusRTUGatewayConnectionsManager.__connections.get(
+            settings.PORT
+        ) is None:
+            # Create a new connection.
+            client = AsyncModbusSerialClient(
+                    port=settings.PORT, 
+                    baudrate=settings.BAUNDRATE, 
+                    bytesize=settings.BYTESIZE, 
+                    parity=settings.PARITY, 
+                    stopbits=settings.STOPBITS, 
+                    time_out=settings.TIME_OUT
+                )
+            # Connect
+            await client.connect()
+            ModbusRTUGatewayConnectionsManager.__connections[
+                settings.PORT: client
+            ]
--- feed_scale/reader.py
+++ feed_scale/reader.py
@@ -22,92 +22,35 @@ class FeedScaleRTUReader(ModbusReader):
             self, 
             length: int, 
             duration: float, 
+            client: AsyncModbusSerialClient, 
             slave: int, 
-            port: str, 
-            baundrate: int = 38400, 
-            bytesize: int = 8, 
-            parity: str = "N", 
-            stopbits: int = 1, 
-            time_out: int = 5
     ) -> None:
         ''' 
         Read data from a RTU slave through a serial port. 
         * param length: number of records read in a call of `read()`.
         * param duration: the duration between two reading in each call of `read().
+        * param client: a connection to a modbus rtu gateway. Please receive through `GatewayManager`.
         * param slave: port number in modbus.
-        * param port: serial port number. Should look like 'COM2', 'COM3'. Please refer to (COM and LPT) section in the Device Manager in Windows.
-        * param baundrate: the baundrate of the modbus device. Please refer to the device document provided by productors. 
-        * param bytesize: modbus setting. Please refer to the device document provided by productors.
-        * param parity: modbus setting. Please refer to the device document provided by productors.
-        * param stopbits: modbus setting. Please refer to the device document provided by productors.
-        * param time_out: modbus setting. Please refer to the device document provided by productors.
         '''
         
-        super().__init__(length, duration, slave)
-        type_check(port, "port", str)
-        type_check(baundrate, "baundrate", int)
-        type_check(bytesize, "bytesize", int)
-        type_check(parity, "parity", str)
-        type_check(stopbits, "stopbits", int)
-        type_check(time_out, "time_out", int)
-        self.PORT = port
-        self.BAUNDRATE = baundrate
-        self.BYTESIZE = bytesize
-        self.PARITY = parity
-        self.STOPBITS = stopbits
-        self.TIME_OUT = time_out
-        self.__master = None
-
-    def __str__(self) -> str:
-        
-        descriptions = [
-            "RTUReader. ", 
-            f"Port: {self.PORT}", 
-            f"Baundrate: {self.BAUNDRATE}", 
-            f"Bytesize: {self.BYTESIZE}", 
-            f"Parity: {self.PARITY}", 
-            f"Stopbits: {self.STOPBITS}", 
-            f"Time out: {self.TIME_OUT}", 
-            f"Slave: {self._SLAVE}"
-        ]
-        return "\n".join(descriptions)
-
-    async def __connect(self) -> bool:
-        ''' Connect to the serial gateway.'''
-
-        self.__master = AsyncModbusSerialClient(
-            port=self.PORT, 
-            baudrate=self.BAUNDRATE, 
-            bytesize=self.BYTESIZE, 
-            parity=self.PARITY, 
-            stopbits=self.STOPBITS, 
-            time_out=self.TIME_OUT
-        )
-        return await self.__master.connect()
-    
-    def __close(self) -> None:
-        ''' Close connection. '''
-
-        self.__master.close()
+        type_check(client, "client", AsyncModbusSerialClient)
+        super().__init__(length, duration, client, slave)
 
     async def read(self) -> pd.DataFrame:
         '''
         Read and return a batch of data. \
         Data attributes: datetime, weight
+        把group問題移到SensorManager?
         '''
-
-        if not await self.__connect():
-            raise ConnectionError("Can not connect to RTU slave")
 
         time_list = []
         weight_list = []
         for _ in range(self._LENGTH_OF_A_BATCH):
             # Get register data
-            data = await self.__master.read_holding_registers(address=0, count=1, slave=self._SLAVE)
+            data = await self._CLIENT.read_holding_registers(address=0, count=1, slave=self._SLAVE)
             # Compute weight and append
             weight_list.append(calculate_weight_from_register(data.registers[0]))
             time_list.append(datetime.now())
             await asyncio.sleep(self._DURATION)
 
-        self.__close()
         return pd.DataFrame({"datetime": time_list, "weight": weight_list})
